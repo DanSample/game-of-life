1e0280b810c866e411054b2cc6b079314442e830
Dan Sample <danssample@gmail.com>
2020-06-16 19:19:43 -0700

    created a grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const numRows = 50;
+const numColumns = 50;
 
 const App = () => {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      // Generate an array with Array and assign its length with numColumns
+      // Array.from will generate the array and initializes the values to 0.
+      rows.push(Array.from(Array(numColumns), () => 0));
+    }
+    return rows;
+  });
+
+  console.log(grid);
   return <div className="App">Hello</div>;
 };
 
