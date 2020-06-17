70607fdadb06b8b74ff7c3079a7c0af0fbc4df08
Dan Sample <danssample@gmail.com>
2020-06-16 20:44:29 -0700

    added immer module to handle state, on click handler for grid squares to handle initial state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import produce from 'immer'; //https://css-tricks.com/using-immer-for-react-state-management/
 
 const numRows = 50;
 const numColumns = 50;
@@ -17,6 +18,8 @@ const App = () => {
     return rows;
   });
 
+  // Created some objects for styling the grid layout
+
   const columnStyle = (x, y) => ({
     width: '20px',
     height: '20px',
@@ -33,7 +36,24 @@ const App = () => {
     <div style={gridStyle}>
       {grid.map((rows, x) =>
         rows.map((col, y) => (
-          <div key={`${x}-${y}`} style={columnStyle(x, y)} />
+          <div
+            key={`${x}-${y}`}
+            /*  Since state is meant to be immutable, I am using the produce() function 
+            from Immer to create a "draft" of the state. This allows the draft of state 
+            to be safely manipulated without disturbing the original state. The draft is
+            given to a messenger then returns the draft to the state. The changes made to 
+            the draft, then get applied to the state. In this way, It does not break the 
+            idea of an immutable state, as the current state does not get updated directly. 
+            */
+            onClick={() => {
+              setGrid(
+                produce(grid, (gridDraft) => {
+                  gridDraft[x][y] = grid[x][y] ? 0 : 1;
+                })
+              );
+            }}
+            style={columnStyle(x, y)}
+          />
         ))
       )}
     </div>
